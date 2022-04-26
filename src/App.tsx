7e0839c314f0ca8266fbe9b00f2f5ee6c47e3e3c
silence_dream <1428482231@qq.com>
2022-04-26 09:44:23 +0800

    移动端像素转换

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,22 @@
 import './App.scss';
+
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
+
+import Layout from '@/pages/Layout';
+import Login from '@/pages/Login';
 function App() {
-  return <div className="app">12312</div>;
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <Switch>
+          {/*https://v5.reactrouter.com/web/api/Route/render-func*/}
+          <Route exact path="/" render={() => <Redirect to="/home" />}></Route>
+          <Route path={'/home'} component={Layout}></Route>
+          <Route path={'/login'} component={Login}></Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,4 +1,12 @@
+import './index.scss';
+
+import { Button } from 'antd-mobile';
 const Login = () => {
-  return <div>登录页面</div>;
+  return (
+    <div>
+      <h1>登录页面</h1>
+      <Button color="primary">按钮</Button>
+    </div>
+  );
 };
 export default Login;
