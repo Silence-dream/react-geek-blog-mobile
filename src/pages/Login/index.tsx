33189575d9df2b76bb4baf8b8927423dafb86acc
silence_dream <1428482231@qq.com>
2022-04-26 10:34:14 +0800

    封装Icon

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,11 +1,14 @@
 import './index.scss';
 
 import { Button } from 'antd-mobile';
+
+import Icon from '@/components/Icon';
 const Login = () => {
   return (
     <div>
       <h1>登录页面</h1>
       <Button color="primary">按钮</Button>
+      <Icon type="iconbtn_like_sel" />
     </div>
   );
 };
