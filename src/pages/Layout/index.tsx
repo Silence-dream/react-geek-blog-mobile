4a3af2b544513c4a7ffcd302f9b3a1c642842d0d
silence_dream <1428482231@qq.com>
2022-04-28 16:05:00 +0800

    Tabbar完成

--- src/pages/Layout/index.tsx
+++ src/pages/Layout/index.tsx
@@ -1,13 +1,64 @@
-import { useSelector } from 'react-redux';
+import { TabBar } from 'antd-mobile';
+import { Route, useHistory, useLocation } from 'react-router-dom';
 
-import { RootState } from '@/types/store';
+import Icon from '@/components/Icon';
 
-export default function Layout() {
-  let state = useSelector((state: RootState) => state);
-  console.log(state);
+// 导入页面组件，配置路由
+import Home from '../Home';
+import Profile from '../Profile';
+import Question from '../Question';
+import Video from '../Video';
+import styles from './index.module.scss';
+
+const tabs = [
+  { path: '/home/index', icon: 'iconbtn_home', text: '首页' },
+  { path: '/home/question', icon: 'iconbtn_qa', text: '问答' },
+  { path: '/home/video', icon: 'iconbtn_video', text: '视频' },
+  { path: '/home/profile', icon: 'iconbtn_mine', text: '我的' },
+];
+
+const Layout = () => {
+  const localtion = useLocation();
+  const history = useHistory();
+  // 切换路由
+  function changeRoute(key: string) {
+    history.push(key);
+  }
   return (
-    <>
-      <h1>Layout</h1>
-    </>
+    <div className={styles.root}>
+      <Route exact path="/home/index">
+        <Home></Home>
+      </Route>
+      <Route path="/home/question">
+        <Question></Question>
+      </Route>
+      <Route path="/home/video">
+        <Video></Video>
+      </Route>
+      <Route path="/home/profile">
+        <Profile></Profile>
+      </Route>
+
+      <TabBar
+        className="tab-bar"
+        activeKey={localtion.pathname}
+        onChange={(key) => changeRoute(key)}
+      >
+        {tabs.map((item) => (
+          <TabBar.Item
+            key={item.path}
+            icon={(active) => (
+              <Icon
+                type={active ? `${item.icon}_sel` : item.icon}
+                className="tab-bar-item-icon"
+              />
+            )}
+            title={item.text}
+          />
+        ))}
+      </TabBar>
+    </div>
   );
-}
+};
+
+export default Layout;
